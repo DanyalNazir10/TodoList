6fd714fb9e72d2d8bf9a7ebbeb8f10b93cc9163d
DanyalNazir10 <danyal.nazir@mergestack.com>
2023-11-13 03:17:15 -0800

    Setup redux saga

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
 import todoReducer from "../../src/features/todo/todoSlice";
+import mySaga from "../../src/sagas";
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: todoReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware),
 });
+
+sagaMiddleware.run();
